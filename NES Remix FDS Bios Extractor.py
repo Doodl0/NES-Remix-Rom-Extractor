af08e289de71c7c6bf1ac832e0a760bc972ca764
Doodl0 <52604696+Doodl0@users.noreply.github.com>
2023-11-12 14:27:34 +0000

    Added bios extractor and header fixer (by Kitrinx)
    Added Bios Extractor
    Added Header fix to Rom Extractor (code by Kitrinx https://github.com/Kitrinx/NES_Header_Repair)
    Added Bios and Rom Extractor into one file

---
+++ NES Remix FDS Bios Extractor.py
@@ -0,0 +1,28 @@
+import os 
+
+#create bios folder
+newpath = r"./bios"  
+
+if not os.path.exists(newpath): 
+    os.makedirs(newpath) 
+
+def fdsbiosfix(x,y): 
+    # load rom 
+    with open(x, 'rb') as f: 
+        data = f.read() 
+    # remove rom data and footer
+    data=data[131136:] 
+    data=data[:-16] 
+    # fix different bytes 
+    data=data[:569] + b"\x85" + data[569 + 1:]
+    data=data[:1030] + b"\x85" + data[1030 + 1:]
+    data=data[:1854] + b"\xA2" + data[1854 + 1:]
+    data=data[:1855] + b"\xB2" + data[1855 + 1:]
+    data=data[:1856] + b"\xCA" + data[1856 + 1:]
+    data=data[:1956] + b"\x4C" + data[1956 + 1:]
+    data=data[:3828] + b"\xA5" + data[3828 + 1:]
+    # save fixed bios
+    with open("bios/"+y, 'wb') as f: 
+        f.write(data)
+
+fdsbiosfix("WUP-FBAJ.bin", "disksys.rom")             
--- NES Remix Rom and Bios Extractor.py
+++ NES Remix Rom and Bios Extractor.py
@@ -4,6 +4,10 @@ newpath = r"./roms"
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
+newpath = r"./bios"  
+if not os.path.exists(newpath): 
+    os.makedirs(newpath) 
+
 def nesfix(x,y):
     # load rom
     with open(x, 'rb') as f:
@@ -24,6 +28,25 @@ def fdsfix(a,b):
     data = data[:-8224]
     # save fixed rom
     with open("roms/"+b, 'wb') as f:
+        f.write(data)
+
+def fdsbiosfix(x,y): 
+    # load rom 
+    with open(x, 'rb') as f: 
+        data = f.read() 
+    # remove rom data and footer
+    data=data[131136:] 
+    data=data[:-16] 
+    # fix different bytes 
+    data=data[:569] + b"\x85" + data[569 + 1:]
+    data=data[:1030] + b"\x85" + data[1030 + 1:]
+    data=data[:1854] + b"\xA2" + data[1854 + 1:]
+    data=data[:1855] + b"\xB2" + data[1855 + 1:]
+    data=data[:1856] + b"\xCA" + data[1856 + 1:]
+    data=data[:1956] + b"\x4C" + data[1956 + 1:]
+    data=data[:3828] + b"\xA5" + data[3828 + 1:]
+    # save fixed bios
+    with open("bios/"+y, 'wb') as f: 
         f.write(data)
 
 
@@ -143,7 +166,14 @@ for i in range (len(romnames)):
         except:
             print("Couldn't find "+romnames[i])
         else:
-            print("Extracted "+romdata[romnames[i]])         
+            print("Extracted "+romdata[romnames[i]])
+
+try: 
+    fdsbiosfix("WUP-FBAJ.bin", "disksys.rom")
+except:
+    print("Couldn't find WUP-FBAJ.bin") 
+else:
+    print("Extracted disksys.rom")           
 
 # Included script by Kitrinx
 
